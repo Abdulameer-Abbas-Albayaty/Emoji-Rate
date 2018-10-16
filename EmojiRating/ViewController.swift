33b7410a51c3a5f03becb7ba4d0ea9ee91f1e5bf
Abdulameer-Abbas-Albayaty <32478718+Abdulameer-Abbas-Albayaty@users.noreply.github.com>
2018-10-16 15:02:23 +0300

    Initial Commit

---
+++ EmojiRating/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmojiRating
+//
+//  Created by Admin on 10/16/18.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
